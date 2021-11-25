bfda4e29b488e8730c2203af80e78d6070f825ab
anseljohn <jjohnanselmo@gmail.com>
2021-11-25 12:46:48 -0500

    Feature exploration : hl, oc, ochl

--- modules/Predictor.py
+++ modules/Predictor.py
@@ -1,12 +1,27 @@
+import math
 import yfinance as yf
 import pandas as pd
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
-hist = yf.download('aapl',period="1d",interval="1m")
-print(hist.values)
-hist['Open'].plot(label="aapl")
-plt.title("AAPL over time: 1d, 1m inc")
+df = yf.download('aapl', period="1d", interval="1m")
+df['4avg'] = df[['Open','Close','High','Low']].mean(axis=1)
+df['hl_avg'] = df[['High','Low']].mean(axis=1)
+print(df)
+
+avgplt = plt.figure(1)
+openplt = plt.figure(2)
+
+df['4avg'].plot(label='avg', c='red', alpha=.150, lw=1)
+df['hl_avg'].plot(label='aapl', c='green', alpha=.150, lw=1)
+plt.title("Open")
 plt.show()
+
+print(df.columns)
